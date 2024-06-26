d297de97e3bb32fb227eb4f60d686faee7e14272
Joost Diepenmaat <joost@zeekat.nl>
2024-06-26 10:34:01 +0200

    Prep jar building, CI checks

---
+++ Makefile
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2024 Jomco B.V.
+# SPDX-FileCopyrightText: 2024 Topsector Logistiek
+# SPDX-FileContributor: Joost Diepenmaat <joost@jomco.nl
+# SPDX-FileContributor: Remco van 't Veer <remco@jomco.nl>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+.PHONY: check working_tree_clean_check test
+
+working_tree_clean_check:
+# ensure no uncommitted stuff
+# git-status --porcelain should print 0 lines.  wc -l counts lines
+# tee /dev/fd/2 prints any uncommitted changes to stderr for logging in CI
+	exit $$(git status --porcelain |tee /dev/fd/2| wc -l)
+
+check: working_tree_clean_check
+	$(MAKE) -C ishare-jwt check
+
+test:
+	$(MAKE) -C ishare-jwt test
---
+++ ishare-jwt/build.clj
@@ -0,0 +1,39 @@
+;;; SPDX-FileCopyrightText: 2024 Jomco B.V.
+;;; SPDX-FileCopyrightText: 2024 Topsector Logistiek
+;;; SPDX-FileContributor: Joost Diepenmaat <joost@jomco.nl
+;;; SPDX-FileContributor: Remco van 't Veer <remco@jomco.nl>
+;;;
+;;; SPDX-License-Identifier: AGPL-3.0-or-later
+
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'org.bdinetwork/ishare-jwt)
+(def version (format "0.1.%s" (b/git-count-revs nil)))
+
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+(def class-dir "target/classes")
+
+;; delay to defer side effects (artifact downloads)
+(def basis (delay (b/create-basis {:project "deps.edn"})))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib       lib
+                :version   version
+                :basis     @basis
+                :src-dirs  ["src"]
+                :pom-data  [[:organization
+                             [:name "BDI Network"]
+                             [:url "https://bdinetwork.org"]]
+                            [:licenses
+                             [:license
+                              [:name "AGPL-3.0-or-later"]
+                              [:url "https://www.gnu.org/licenses/agpl-3.0.en.html"]]]]})
+  (b/copy-dir {:src-dirs   ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file  jar-file}))
